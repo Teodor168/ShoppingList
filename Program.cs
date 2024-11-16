a16d0b8b00ea1f67c0350864a70d317d1589f6a6
Tedo <159997664+Teodor168@users.noreply.github.com>
2024-11-16 16:58:39 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ShoppingList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> shoppingList = new LinkedList<string>();
+            shoppingList.AddFirst("Хляб");
+            shoppingList.AddLast("Мляко");
+            shoppingList.AddFirst("Яйца");
+            shoppingList.AddLast("Плодове");
+
+            Console.WriteLine("Списък за пазаруване:");
+            PrintList(shoppingList);
+
+            shoppingList.RemoveFirst();
+            Console.WriteLine();
+            Console.WriteLine("След купуване на първия елемент:");
+            
+            PrintList(shoppingList);
+
+            shoppingList.RemoveLast();
+            Console.WriteLine();
+            Console.WriteLine("След купуване на последния елемент:");
+            
+            PrintList(shoppingList);
+        }
+
+        static void PrintList(LinkedList<string> list)
+        {
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
